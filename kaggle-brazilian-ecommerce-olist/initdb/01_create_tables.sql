8e078139c8e6be8735c6e75ff9e9a1facdc9385e
Pricilla Patricia de Aragao <pricillapatricia@gmail.com>
2022-07-26 08:33:05 -0400

    kaggle-brazilian-ecommerce-olist Add products import

--- kaggle-brazilian-ecommerce-olist/initdb/01_create_tables.sql
+++ kaggle-brazilian-ecommerce-olist/initdb/01_create_tables.sql
@@ -21,3 +21,15 @@ CREATE TABLE IF NOT EXISTS sellers(
     city TEXT NOT NULL,
     state CHAR(2) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS products(
+    id CHAR(32) PRIMARY KEY,
+    category_name TEXT NOT NULL,
+    name_length INT,
+    description_length INT,
+    photos_qty INT,
+    weight_g INT,
+    length_cm INT,
+    height_cm INT,
+    width_cm INT
+);
