c6dd9bba9f67d410989e3668f00d379868489630
Pricilla Patricia de Aragao <pricillapatricia@gmail.com>
2022-07-26 07:14:46 -0400

    Update kaggle-brazilian-ecommerce-olist customers table

--- kaggle-brazilian-ecommerce-olist/initdb/01_create_tables.sql
+++ kaggle-brazilian-ecommerce-olist/initdb/01_create_tables.sql
@@ -1,7 +1,7 @@
-CREATE TABLE customers(
-    customer_id CHAR(32) PRIMARY KEY,
-    customer_unique_id CHAR(32) NOT NULL,
-    customer_zip_code_prefix CHAR(5) NOT NULL,
-    customer_city TEXT NOT NULL,
-    customer_state CHAR(2) NOT NULL
+CREATE TABLE IF NOT EXISTS customers(
+    id CHAR(32) PRIMARY KEY,
+    unique_id CHAR(32) NOT NULL,
+    zip_code_prefix CHAR(5) NOT NULL,
+    city TEXT NOT NULL,
+    state CHAR(2) NOT NULL
 );
