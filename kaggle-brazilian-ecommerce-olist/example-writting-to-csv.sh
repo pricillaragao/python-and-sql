c09d1e98c765b5012a21d909d28a64a2af921e91
Pricilla Patricia de Aragao <pricillapatricia@gmail.com>
2022-07-27 10:05:55 -0400

    kaggle-brazilian-ecommerce-olist Start example-writting-to-csv

---
+++ kaggle-brazilian-ecommerce-olist/example-writting-to-csv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker compose up -d
+# TODO(pricilla): look for a more efficient way of waiting until the container is healthy
+sleep 20
+poetry run python -m example_writting_to_csv
+docker compose down
